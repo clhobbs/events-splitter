299def6095cea6fe6c8525a22dededae11f62681
Colleen Hobbs <132529006+clhobbs@users.noreply.github.com>
2023-06-29 12:29:59 -0400

    test: start/stop docker in before/after hook (#12)
    * start stop docker in before after hook
    
    * handle case when no testreport folder

--- test/test.js
+++ test/test.js
@@ -3,14 +3,18 @@ const { expect } = require('chai');
 const fs = require('fs');
 const readline = require('readline');
 const path = require('path');
+const spawn = require('child_process').spawn;
 
 const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('./test/events.db');
 
+let extendedTimeout = 90000;  // for long-running hooks or tests
+
 // for reporting test case failures
 let errorMsg = '';
 let errorData = '';
-const failedTestsFolder = path.join(__dirname, '../testreport/failedTests');
+const testreportFolder = path.join(__dirname, '../testreport');
+const failedTestsFolder = path.join(testreportFolder, 'failedTests');
  
 /**
  * Insert each line from an events file into an events array.
@@ -63,6 +67,29 @@ async function arrayToDb(arr) {
   });
 }
 
+/**
+ * Run a command as a child process (e.g 'docker compose up'). Spawn is used in this method so that
+ * stdout is streamed instead of buffered.
+ * 
+ * @param {*} cmd the command to run, e.g 'docker'
+ * @param {*} args an array of args used by the cmd, e.g ['compose', 'up']
+ */
+async function runCommand(cmd, args) {
+  await new Promise((resolve, reject) => {
+    let child = spawn(cmd, args);
+    // display stdout on screen
+    child.stdout.on('data', function (data) {   process.stdout.write(data.toString());  });
+
+    // display stderr on screen
+    child.stderr.on('data', function (data) {   process.stdout.write(data.toString());  });
+
+    child.on('close', function (code) { 
+      console.log('finished with code ' + code);
+      resolve();
+    });
+  });  
+}
+
 describe('Events Splitter', function() {
 
   let agentEvents = [];
@@ -70,25 +97,47 @@ describe('Events Splitter', function() {
   let target2Events = [];
 
   before(async function() {
+    this.timeout(extendedTimeout);
 
+    // start docker
+    console.log('starting docker containers... this can take a minute');
+    await runCommand('npm', ['run', 'docker-restart']);
+
+    // wait 5 seconds for the Splitter to complete and the Target events.log files to be written
+    await new Promise((res) => setTimeout(res, 5000));
+
+    // copy the Target events files to the testreport folder
+    console.log('copying Target event files');
+    if (!fs.existsSync(testreportFolder)) await fs.promises.mkdir(testreportFolder);
+    await runCommand('docker', ['cp', 'Target-1:events.log', 'testreport/target1_events.log']);
+    await runCommand('docker', ['cp', 'Target-2:events.log', 'testreport/target2_events.log']);
+
+    // fill up the arrays with all rows from the agent / target1 / target2 files
     const agentFile = path.join(__dirname, '../agent/inputs/large_1M_events.log');
     const target1File = path.join(__dirname, '../testreport/target1_events.log');
     const target2File = path.join(__dirname, '../testreport/target2_events.log');
   
-    // fill up the arrays with all rows from the agent / target1 / target2 files
     await Promise.all([
         fileToArray(agentFile, agentEvents, 'Agent'),
         fileToArray(target1File, target1Events, 'Target1'),
         fileToArray(target2File, target2Events, 'Target2')
     ]);
-
   });
 
   after(async function() {
+    this.timeout(extendedTimeout);
+
     // copy the db to the testreports folder, clear out the data and close the db
     await fs.promises.copyFile('./test/events.db', './testreport/events.db');
     db.run("DELETE FROM events");
     db.close();    
+
+    // copy console logs from the containers to the testreport folder
+    await runCommand('npm', ['run', 'copy-logs']);
+
+    // shutdown docker containers
+    console.log('stopping docker...');
+    await runCommand('npm', ['run', 'docker-down']);
   });
 
   describe('Validate events', async function() {
